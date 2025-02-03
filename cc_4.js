721c2cabff3d5faeea601121402e83ce095eca32
Jasonmmill <jasonmatthewmill@gmail.com>
2025-02-02 20:26:04 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -4,4 +4,13 @@ let finalAmount = purchaseAmount *= 0.8
 if (purchaseAmount > 100) {
     console.log(finalAmount)
 }
- console.log(`Final amount after discount: $${purchaseAmount}`)
+console.log(`Final amount after discount: $${purchaseAmount}`)
+
+// Task 2: For Loop 
+
+let sales = [ 120, 85, 200, 150, 90 ]
+let totalSales = 0
+for (let i = 0; i < sales.length; i++) {
+    totalSales += sales[i];
+}
+console.log(`Total sales: $${totalSales}`)
