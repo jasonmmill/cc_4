c9365dbec0d014144df9e397a2f598f57f87b059
Jasonmmill <jasonmatthewmill@gmail.com>
2025-02-02 19:56:44 -0500

    Task 1 - Customer Discounts

---
+++ cc_4.js
@@ -0,0 +1,7 @@
+// Task 1: If Statements
+let purchaseAmount = 125
+let finalAmount = purchaseAmount *= 0.8
+if (purchaseAmount > 100) {
+    console.log(finalAmount)
+}
+ console.log(`Final amount after discount: $${purchaseAmount}`)
